3779116921b7246cccd27d9dda1cfdd7bc742949
JoakoMasq <joamasquere@hotmail.com>
2023-05-18 21:59:22 -0300

    cambio

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var readline = require('readline-sync')
 
 let n1 = readline.question("Ingrese su nombre:   ")
 
-    console.log("Se registro el acceso de: " + n1)
+    console.log("Se registro el acceso : " + n1)
 
 
