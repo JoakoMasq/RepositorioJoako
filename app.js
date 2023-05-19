052f77434315d63fe480d51a6e666d48f61d5e90
JoakoMasq <joamasquere@hotmail.com>
2023-05-18 21:51:26 -0300

    prueba

---
+++ app.js
@@ -0,0 +1,8 @@
+var readline = require('readline-sync')
+
+
+let n1 = readline.question("Ingrese su nombre:   ")
+
+    console.log("Se registro el acceso de: " + n1)
+
+
---
+++ app2.js
@@ -0,0 +1,16 @@
+let nota=10;
+
+    console.log("Su nota es:  " + nota)
+
+    if  ((nota>=8) && (nota<=10)){
+    console.log("Excelente");
+}
+else if (nota>=6){
+    console.log("Aprobado");
+}
+else if ((nota<6) && (nota>=0)){
+    console.log("No aprobado");
+}
+else {
+    console.log("Nota no valida");
+}
